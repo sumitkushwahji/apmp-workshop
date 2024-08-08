8fbe8a1949f68611350fd96a49b52d7f87b41a54
sumitkushwahji <sumitkushwahji@gmail.com>
2024-08-08 12:54:25 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
